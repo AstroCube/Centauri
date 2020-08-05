98a35868f9b257249a46196ec402c7c846320686
yusshu <yushuverduras1309@gmail.com>
2020-08-05 16:53:06 -0500

    Fix Friendship model

--- API/core/src/main/java/net/astrocube/api/core/virtual/friend/Friendship.java
+++ API/core/src/main/java/net/astrocube/api/core/virtual/friend/Friendship.java
@@ -1,8 +1,7 @@
 package net.astrocube.api.core.virtual.friend;
 
 import net.astrocube.api.core.model.ModelProperties;
-import net.astrocube.api.core.virtual.server.ServerDoc;
 
 @ModelProperties.RouteKey("friends")
 @ModelProperties.Cache(0)
-public interface Friendship extends ServerDoc.Complete {}
+public interface Friendship extends FriendshipDoc.Complete {}
