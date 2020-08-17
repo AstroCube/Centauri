f640149eea378261743b79379138733ccce4e72b
yusshu <yushuverduras1309@gmail.com>
2020-08-17 13:19:10 -0500

    Add add sub command

--- Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/command/friends/AddSubCommand.java
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/command/friends/AddSubCommand.java
@@ -1,11 +1,14 @@
 package net.astrocube.commons.bukkit.command.friends;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.Inject;
 import me.fixeddev.ebcm.parametric.annotation.ACommand;
 import me.fixeddev.ebcm.parametric.annotation.Injected;
 import me.yushust.message.core.MessageProvider;
 import net.astrocube.api.core.friend.FriendshipHandler;
 import net.astrocube.api.core.service.find.FindService;
+import net.astrocube.api.core.service.query.QueryService;
 import net.astrocube.api.core.virtual.user.User;
 import net.astrocube.commons.core.utils.Callbacks;
 import org.bukkit.OfflinePlayer;
@@ -18,6 +21,8 @@ public class AddSubCommand {
     private @Inject FriendshipHandler friendshipHandler;
     private @Inject FindService<User> userFindService;
     private @Inject FriendCommandValidator friendCommandValidator;
+    private @Inject QueryService<User> userQueryService;
+    private @Inject ObjectMapper objectMapper;
 
     @ACommand(names = "")
     public boolean execute(@Injected(true) Player player, OfflinePlayer target) {
@@ -26,15 +31,35 @@ public class AddSubCommand {
             return true;
         }
 
+        userFindService.find(player.getDatabaseIdentifier())
+            .callback(Callbacks.applyCommonErrorHandler("Find userdata of " + player.getName(),
+                user -> {
 
-        userFindService.find(player.getDatabaseIdentifier()).callback(Callbacks.applyCommonErrorHandler(
-"Find userdata of " + player.getName(),
-            user -> {
+                    ObjectNode filter = objectMapper.createObjectNode()
+                            .put("username", target.getName());
 
-                // TODO: Find the offlineplayer by name using UserFindService
+                    userQueryService.query(filter).callback(Callbacks.applyCommonErrorHandler("Find userdata of " + target.getName(), result -> {
 
-            }
-        ));
+                        User targetUser = result.getFoundModels().iterator().next();
+
+                        if (friendCommandValidator.checkAlreadySent(player, user, targetUser)) {
+                            return;
+                        }
+
+                        if (friendCommandValidator.checkAlreadyFriends(player, user, targetUser)) {
+                            return;
+                        }
+
+
+                        player.sendMessage(
+                                messageProvider.getMessage(player, "commons-friend-request-sent")
+                                    .replace("%%receiver%%", target.getName())
+                        );
+
+                    }));
+
+                }
+            ));
 
         return true;
 
--- Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/command/friends/FriendCommandValidator.java
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/command/friends/FriendCommandValidator.java
@@ -1,19 +1,29 @@
 package net.astrocube.commons.bukkit.command.friends;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import me.yushust.message.core.MessageProvider;
 import net.astrocube.api.core.redis.Redis;
+import net.astrocube.api.core.service.query.QueryService;
+import net.astrocube.api.core.virtual.friend.Friendship;
 import net.astrocube.api.core.virtual.user.User;
 import net.astrocube.commons.bukkit.utils.ChatAlertLibrary;
+import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import redis.clients.jedis.Jedis;
+
+import java.util.Collection;
+import java.util.logging.Level;
 
 @Singleton
 public class FriendCommandValidator {
 
     private @Inject MessageProvider<Player> messageProvider;
+    private @Inject QueryService<Friendship> friendshipQueryService;
+    private @Inject ObjectMapper objectMapper;
     private @Inject Redis redis;
 
     /**
@@ -57,6 +67,50 @@ public class FriendCommandValidator {
                 messageProvider.getMessage(player, "commons-friend-already-sent")
         );
         return true;
+    }
+
+    /**
+     * Checks if a friendship of a player and target exists,
+     * in that case, an alert is sent to the player
+     * @param player The issuer
+     * @param issuer The issuer user data
+     * @param receiver The receiver
+     * @return True if a friendship exists
+     */
+    public boolean checkAlreadyFriends(Player player, User issuer, User receiver) {
+
+        ObjectNode filter = objectMapper.createObjectNode();
+        filter.putArray("$or")
+                .add(
+                        objectMapper.createObjectNode()
+                            .put("issuer", issuer.getId())
+                            .put("receiver", receiver.getId())
+                )
+                .add(
+                        objectMapper.createObjectNode()
+                            .put("issuer", receiver.getId())
+                            .put("receiver", issuer.getId())
+                );
+
+        Collection<Friendship> friendships;
+
+        try {
+            friendships = friendshipQueryService.querySync(filter).getFoundModels();
+        } catch (Exception exception) {
+            ChatAlertLibrary.alertChatError(player, exception.getMessage());
+            Bukkit.getLogger().log(Level.SEVERE, "An error has appeared while querying friendships", exception);
+            return true;
+        }
+
+        if (!friendships.isEmpty()) {
+            ChatAlertLibrary.alertChatError(
+                    player,
+                    messageProvider.getMessage(player, "commons-friend-already-friends")
+            );
+        }
+
+        return !friendships.isEmpty();
+
     }
 
 }
