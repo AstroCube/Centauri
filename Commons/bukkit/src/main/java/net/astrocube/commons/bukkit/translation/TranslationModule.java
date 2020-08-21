282dd9dbb3bcf21f19feea71f2efb6dc382da333
yusshu <yushuverduras1309@gmail.com>
2020-08-21 13:15:03 -0500

    Update lang-lib version

--- Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/translation/TranslationModule.java
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/translation/TranslationModule.java
@@ -4,8 +4,10 @@ import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import me.fixeddev.inject.ProtectedModule;
 import me.yushust.message.core.MessageProvider;
+import me.yushust.message.core.MessageProviderBuilder;
+import me.yushust.message.core.ProvideStrategy;
 import me.yushust.message.format.bukkit.BukkitMessageProviders;
-import me.yushust.message.format.bukkit.yaml.YamlFileLoader;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
@@ -13,10 +15,15 @@ public class TranslationModule extends ProtectedModule {
 
     @Provides @Singleton
     public MessageProvider<Player> provideMessageProvider(Plugin plugin, CoreLanguageProvider languageProvider) {
-        MessageProvider<Player> messageProvider = BukkitMessageProviders.getPlayerMessageProvider(
-                plugin, new YamlFileLoader(plugin, plugin.getDataFolder()), languageProvider, "lang_%lang%.yml"
-        );
-        messageProvider.getInterceptor().addInterceptor(new ColorMessageInterceptor());
-        return messageProvider;
+        return MessageProviderBuilder.<Player>create()
+                .withLoadSource(BukkitMessageProviders.createPluginLoadSource(plugin))
+                .usingLanguageProvider(languageProvider)
+                .defaultLanguage("es")
+                .nodeFileLoader(BukkitMessageProviders.createYamlFileLoader(plugin))
+                .fileFormat("lang_%lang%.yml")
+                .withMessageConsumer(CommandSender::sendMessage)
+                .provideStrategy(ProvideStrategy.RETURN_PATH)
+                .addInterceptor(new ColorMessageInterceptor())
+                .build();
     }
 }
