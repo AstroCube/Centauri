c06ced2808a73f10453cd04b6de373c37505a766
yusshu <yushuverduras1309@gmail.com>
2020-08-22 12:30:16 -0500

    Register friends command

--- Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/loader/CommandLoader.java
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/loader/CommandLoader.java
@@ -8,23 +8,23 @@ import me.fixeddev.ebcm.bukkit.BukkitCommandManager;
 import me.fixeddev.ebcm.bukkit.BukkitMessenger;
 import me.fixeddev.ebcm.bukkit.parameter.provider.BukkitModule;
 import me.fixeddev.ebcm.parameter.provider.ParameterProviderRegistry;
-import me.fixeddev.ebcm.parametric.ParametricCommandBuilder;
-import me.fixeddev.ebcm.parametric.ReflectionParametricCommandBuilder;
 import net.astrocube.api.core.loader.Loader;
+import net.astrocube.commons.bukkit.command.FriendsCommand;
 import net.astrocube.commons.bukkit.command.LoginCommand;
+import net.astrocube.commons.bukkit.command.builder.InjectionParametricCommandBuilder;
 import org.bukkit.plugin.Plugin;
 
 public class CommandLoader implements Loader {
 
+    private @Inject InjectionParametricCommandBuilder commandBuilder;
     private @Inject Plugin plugin;
-
     private @Inject LoginCommand loginCommand;
+    private @Inject FriendsCommand friendsCommand;
 
     @Override
     public void load() {
 
         ParameterProviderRegistry registry = ParameterProviderRegistry.createRegistry();
-        ParametricCommandBuilder commandBuilder = new ReflectionParametricCommandBuilder();
         registry.installModule(new BukkitModule());
 
         CommandManager commandManager = new SimpleCommandManager(new BukkitAuthorizer(),
@@ -35,6 +35,8 @@ public class CommandLoader implements Loader {
         if (plugin.getConfig().getBoolean("authentication.enabled")) {
             bukkitManager.registerCommands(commandBuilder.fromClass(loginCommand));
         }
+
+        bukkitManager.registerCommands(commandBuilder.fromClass(friendsCommand));
 
     }
 
