9072c16a89647230cd209d80c32892a89812423c
Ian Castaño <ianfe@martina.com.co>
2020-08-21 23:26:20 -0500

    Created lobby boilerplate

---
+++ Lobby/src/main/java/net/astrocube/lobby/loader/InjectionLoaderModule.java
@@ -0,0 +1,12 @@
+package net.astrocube.lobby.loader;
+
+import me.fixeddev.inject.ProtectedModule;
+
+public class InjectionLoaderModule extends ProtectedModule {
+
+    @Override
+    public void configure() {
+
+    }
+
+}
---
+++ Lobby/src/main/java/net/astrocube/lobby/loader/LobbyLoader.java
@@ -0,0 +1,12 @@
+package net.astrocube.lobby.loader;
+
+import net.astrocube.api.core.loader.Loader;
+
+public class LobbyLoader implements Loader {
+
+    @Override
+    public void load() {
+
+    }
+
+}
