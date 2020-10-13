15e540945ca47c48130932394dc6dc1d34097faa
Ian Castaño <ianfe@martina.com.co>
2020-10-13 13:52:23 -0500

    Add match state updater

---
+++ API/bukkit/src/main/java/net/astrocube/api/bukkit/game/match/MatchStateUpdater.java
@@ -0,0 +1,16 @@
+package net.astrocube.api.bukkit.game.match;
+
+import net.astrocube.api.bukkit.virtual.game.match.Match;
+import net.astrocube.api.bukkit.virtual.game.match.MatchDoc;
+
+public interface MatchStateUpdater {
+
+    /**
+     * Update certain match at the database and emit an event
+     * to make bukkit know about the update.
+     * @param match to be updated
+     * @param status to be updated
+     */
+    void updateMatch(Match match, MatchDoc.Status status);
+
+}
