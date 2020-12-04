4e8b38c15a2c0caca302f7804adcaa55e7494581
pixeldev <amiranda8695@gmail.com>
2020-12-04 12:01:43 -0600

    Adapted to guice instance provider.

--- Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/loader/CommandLoader.java
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/loader/CommandLoader.java
@@ -1,22 +1,32 @@
 package net.astrocube.commons.bukkit.loader;
 
 import com.google.inject.Inject;
+import com.google.inject.Injector;
+
 import me.fixeddev.commandflow.CommandManager;
-import me.fixeddev.commandflow.SimpleCommandManager;
-import me.fixeddev.commandflow.bukkit.BukkitAuthorizer;
+import me.fixeddev.commandflow.annotated.AnnotatedCommandTreeBuilder;
+import me.fixeddev.commandflow.annotated.AnnotatedCommandTreeBuilderImpl;
+import me.fixeddev.commandflow.annotated.builder.AnnotatedCommandBuilderImpl;
+import me.fixeddev.commandflow.annotated.part.PartInjector;
+import me.fixeddev.commandflow.annotated.part.SimplePartInjector;
+import me.fixeddev.commandflow.annotated.part.defaults.DefaultsModule;
 import me.fixeddev.commandflow.bukkit.BukkitCommandManager;
 import me.fixeddev.commandflow.bukkit.factory.BukkitModule;
+import me.fixeddev.commandflow.translator.DefaultTranslator;
+
 import net.astrocube.api.core.loader.Loader;
 import net.astrocube.commons.bukkit.command.FriendsCommand;
 import net.astrocube.commons.bukkit.command.LoginCommand;
 import net.astrocube.commons.bukkit.command.MatchCommand;
-import net.astrocube.commons.bukkit.command.builder.InjectionParametricCommandBuilder;
+import net.astrocube.commons.bukkit.command.flow.CoreCommandLanguageProvider;
+
 import org.bukkit.plugin.Plugin;
 
 public class CommandLoader implements Loader {
 
-    private @Inject InjectionParametricCommandBuilder commandBuilder;
     private @Inject Plugin plugin;
+    private @Inject CoreCommandLanguageProvider coreCommandLanguageProvider;
+    private @Inject Injector injector;
     private @Inject LoginCommand loginCommand;
     private @Inject FriendsCommand friendsCommand;
     private @Inject MatchCommand matchCommand;
@@ -24,21 +34,25 @@ public class CommandLoader implements Loader {
     @Override
     public void load() {
 
+        CommandManager commandManager = new BukkitCommandManager(plugin.getName());
+        commandManager.setTranslator(new DefaultTranslator(coreCommandLanguageProvider));
 
-        ParameterProviderRegistry registry = ParameterProviderRegistry.createRegistry();
-        registry.installModule(new BukkitModule());
+        PartInjector partInjector = new SimplePartInjector();
 
-        CommandManager commandManager = new SimpleCommandManager(new BukkitAuthorizer(),
-                new BukkitMessenger(),
-                registry);
-        BukkitCommandManager bukkitManager = new BukkitCommandManager(commandManager, plugin.getName());
+        partInjector.install(new BukkitModule());
+        partInjector.install(new DefaultsModule());
+
+        AnnotatedCommandTreeBuilder treeBuilder = new AnnotatedCommandTreeBuilderImpl(
+                new AnnotatedCommandBuilderImpl(partInjector),
+                (clazz, parent) -> injector.getInstance(clazz)
+        );
+
+        commandManager.registerCommands(treeBuilder.fromClass(friendsCommand));
+        commandManager.registerCommands(treeBuilder.fromClass(matchCommand));
 
         if (plugin.getConfig().getBoolean("authentication.enabled")) {
-            bukkitManager.registerCommands(commandBuilder.fromClass(loginCommand));
+            commandManager.registerCommands(treeBuilder.fromClass(loginCommand));
         }
-
-        bukkitManager.registerCommands(commandBuilder.fromClass(friendsCommand));
-        bukkitManager.registerCommands(commandBuilder.fromClass(matchCommand));
 
     }
 
