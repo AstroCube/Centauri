b7d7218e10c38c9969fe4855180c4779a1319378
Ian Castaño <ianfe@martina.com.co>
2020-12-07 22:20:25 -0500

    Partially rewritten gamemap model

--- Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/virtual/GameMapModelModule.java
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/virtual/GameMapModelModule.java
@@ -3,6 +3,7 @@ package net.astrocube.commons.bukkit.virtual;
 import com.google.inject.TypeLiteral;
 import me.fixeddev.inject.ProtectedModule;
 import net.astrocube.api.bukkit.virtual.game.map.GameMap;
+import net.astrocube.api.bukkit.virtual.game.map.GameMapDoc;
 import net.astrocube.api.core.utils.ResolvableType;
 import net.astrocube.commons.core.model.binder.ModelBinderModule;
 import net.astrocube.commons.core.service.RedisModelService;
@@ -11,9 +12,9 @@ public class GameMapModelModule extends ProtectedModule implements ModelBinderMo
 
     @Override
     protected void configure() {
-        bindModel(GameMap.class, GameMap.class, model -> {
-            TypeLiteral<RedisModelService<GameMap, GameMap>> serviceTypeLiteral =
-                    new ResolvableType<RedisModelService<GameMap, GameMap>>(){};
+        bindModel(GameMap.class, GameMapDoc.Partial.class, model -> {
+            TypeLiteral<RedisModelService<GameMap, GameMapDoc.Partial>> serviceTypeLiteral =
+                    new ResolvableType<RedisModelService<GameMap, GameMapDoc.Partial>>(){};
             model.bind(serviceTypeLiteral);
         });
     }
