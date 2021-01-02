aec1b5f4ab06434514a249af64c79a4f0a3e9a0b
Ian Castaño <ianfe@martina.com.co>
2021-01-01 19:01:12 -0500

    Added configuration

--- Commons/bungee/src/main/java/net/astrocube/commons/bungee/loader/InjectionModule.java
+++ Commons/bungee/src/main/java/net/astrocube/commons/bungee/loader/InjectionModule.java
@@ -3,12 +3,14 @@ package net.astrocube.commons.bungee.loader;
 import me.fixeddev.inject.ProtectedModule;
 import net.astrocube.commons.bungee.configuration.PluginConfigurationProvider;
 import net.astrocube.commons.bungee.server.ServerModule;
+import net.astrocube.commons.core.CommonsModule;
 import net.md_5.bungee.config.Configuration;
 
 public class InjectionModule extends ProtectedModule {
 
     @Override
     public void configure() {
+        install(new CommonsModule());
         install(new LoaderModule());
         install(new ServerModule());
         bind(Configuration.class).toProvider(PluginConfigurationProvider.class);
