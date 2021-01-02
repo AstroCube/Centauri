80f01ba6496b555666a4e7ebce2004aea48625b0
Ian Castaño <ianfe@martina.com.co>
2021-01-02 00:11:04 -0500

    ?

--- Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/game/match/control/CoreTeamBalancer.java
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/game/match/control/CoreTeamBalancer.java
@@ -4,12 +4,68 @@ import net.astrocube.api.bukkit.game.match.control.TeamBalancer;
 import net.astrocube.api.bukkit.game.matchmaking.MatchAssignable;
 import net.astrocube.api.bukkit.virtual.game.match.MatchDoc;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class CoreTeamBalancer implements TeamBalancer {
+
     @Override
     public Set<MatchDoc.Team> balanceTeams(Set<MatchAssignable> assignations) {
+        /*
+        List<MatchDoc.Team> teams = new ArrayList<>();
+        List<MatchDoc.Team> finalTeams = new ArrayList<>();
+
+        int maxMembers = 1;
+        int teamIndex = 0;
+        int partyMembersCount = 0;
+
+        for (MatchAssignable assignable : assignations) {
+
+            Set<String> members = new HashSet<>(assignable.getInvolved());
+            members.add(assignable.getResponsible());
+
+            if (members.size() <= maxMembers) {
+
+                MatchDoc.Team team = teams.get(teamIndex);
+
+                int teamMembersCount = team.getMembers().size() + members.size();
+
+                if (teamMembersCount <= maxMembers) {
+                    members.forEach(member -> team.getMembers().add(generateMember(member)));
+
+                    if (teamMembersCount == maxMembers) {
+                        teamIndex++;
+                    }
+
+                    partyMembersCount += members.size();
+
+                }
+
+                finalTeams.add(team);
+
+            }
+
+        }
+         */
         return new HashSet<>();
     }
+
+    private MatchDoc.TeamMember generateMember(String id) {
+        return new MatchDoc.TeamMember() {
+            @Override
+            public String getUser() {
+                return id;
+            }
+
+            @Override
+            public boolean isActive() {
+                return true;
+            }
+
+            @Override
+            public Date getJoinedAt() {
+                return new Date();
+            }
+        };
+    }
+
 }
