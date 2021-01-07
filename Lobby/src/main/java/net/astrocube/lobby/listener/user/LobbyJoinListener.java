8401eaa89dcea5ee63b2dafccf9bbf9599bd444c
Ian Castaño <ianfe@martina.com.co>
2021-01-07 16:14:11 -0500

    Added potion and teleport

--- Lobby/src/main/java/net/astrocube/lobby/listener/user/LobbyJoinListener.java
+++ Lobby/src/main/java/net/astrocube/lobby/listener/user/LobbyJoinListener.java
@@ -4,19 +4,53 @@ import com.google.inject.Inject;
 import net.astrocube.api.bukkit.lobby.event.LobbyJoinEvent;
 import net.astrocube.api.bukkit.lobby.hide.HideJoinProcessor;
 import net.astrocube.api.bukkit.lobby.hotbar.LobbyHotbarProvider;
+import org.bukkit.Bukkit;
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class LobbyJoinListener implements Listener {
 
     private @Inject HideJoinProcessor hideJoinProcessor;
     private @Inject LobbyHotbarProvider lobbyHotbarProvider;
+    private @Inject Plugin plugin;
 
     @EventHandler
     public void onLobbyJoin(LobbyJoinEvent event) {
 
+        Player player = event.getPlayer();
+
         hideJoinProcessor.process(event.getUser());
-        lobbyHotbarProvider.setup(event.getUser(), event.getPlayer());
+        lobbyHotbarProvider.setup(event.getUser(), player);
+
+        player.teleport(
+                new Location(
+                        Bukkit.getWorlds().get(0),
+                        plugin.getConfig().getInt("spawn.x", 0),
+                        plugin.getConfig().getInt("spawn.y", 0),
+                        plugin.getConfig().getInt("spawn.z", 0),
+                        plugin.getConfig().getInt("spawn.yaw", 0),
+                        plugin.getConfig().getInt("spawn.pitch", 0)
+                )
+        );
+
+        plugin.getConfig().getStringList("ambiental.effects").forEach(effect -> {
+
+            try {
+                PotionEffectType potionEffectType = PotionEffectType.getByName(effect);
+                player.addPotionEffect(
+                        new PotionEffect(potionEffectType, Integer.MAX_VALUE, 1, true, false)
+                );
+            } catch (IllegalArgumentException e) {
+                plugin.getLogger().warning("Could not find effect type " + effect);
+            }
+
+        });
 
     }
 
