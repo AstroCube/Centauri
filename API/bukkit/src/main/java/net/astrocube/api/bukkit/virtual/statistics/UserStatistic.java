27b2eec08cf171bf05f8137b482f590ae0cc555f
cassha <notcacha1998@gmail.com>
2021-02-02 14:15:44 -0300

    Update RouteKey from UserStatistic

--- API/bukkit/src/main/java/net/astrocube/api/bukkit/virtual/statistics/UserStatistic.java
+++ API/bukkit/src/main/java/net/astrocube/api/bukkit/virtual/statistics/UserStatistic.java
@@ -2,6 +2,6 @@ package net.astrocube.api.bukkit.virtual.statistics;
 
 import net.astrocube.api.core.model.ModelProperties;
 
-@ModelProperties.RouteKey("user-statistic")
+@ModelProperties.RouteKey("stats")
 @ModelProperties.Cache
 public interface UserStatistic<T> extends UserStatisticDoc.Complete<T> {}
