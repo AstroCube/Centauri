e38561a4532299cc2ca039fb240fa00457aa4835
Ian Castaño <ianfe@martina.com.co>
2021-02-09 16:37:13 -0500

    ?

--- Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/board/ScoreboardModule.java
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/board/ScoreboardModule.java
@@ -2,14 +2,13 @@ package net.astrocube.commons.bukkit.board;
 
 import me.fixeddev.inject.ProtectedModule;
 import net.astrocube.api.bukkit.board.ScoreboardManagerProvider;
-import team.unnamed.uboard.ScoreboardManager;
 
 public class ScoreboardModule extends ProtectedModule {
 
     @Override
     public void configure() {
         bind(ScoreboardManagerProvider.class).to(CoreScoreboardManagerProvider.class);
-        bind(ScoreboardManagerProvider.class);
+        expose(ScoreboardManagerProvider.class);
     }
 
 }
