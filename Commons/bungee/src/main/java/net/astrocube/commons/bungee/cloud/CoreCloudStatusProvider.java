d91ed21d9fba9316d54248c440560ce03e042492
Ian Castaño <ianfe@martina.com.co>
2021-02-09 08:30:19 -0500

    Fixed bungee overriding

--- Commons/bungee/src/main/java/net/astrocube/commons/bungee/cloud/CoreCloudStatusProvider.java
+++ Commons/bungee/src/main/java/net/astrocube/commons/bungee/cloud/CoreCloudStatusProvider.java
@@ -1,5 +1,7 @@
 package net.astrocube.commons.bungee.cloud;
 
+import cloud.timo.TimoCloud.api.TimoCloudAPI;
+import cloud.timo.TimoCloud.api.objects.PlayerObject;
 import com.google.inject.Singleton;
 import net.astrocube.api.core.cloud.CloudStatusProvider;
 import net.md_5.bungee.api.ProxyServer;
@@ -15,6 +17,23 @@ public class CoreCloudStatusProvider implements CloudStatusProvider {
     @Override
     public int getOnline() {
         return -1;
+    }
+
+    @Override
+    public boolean hasAliveSession(String player) {
+        return TimoCloudAPI.getUniversalAPI().getPlayer(player).isOnline();
+    }
+
+    @Override
+    public String getPlayerServer(String player) {
+
+        PlayerObject playerObject = TimoCloudAPI.getUniversalAPI().getPlayer(player);
+
+        if (!playerObject.isOnline()) {
+            return "";
+        }
+
+        return playerObject.getServer().getName();
     }
 
 }
