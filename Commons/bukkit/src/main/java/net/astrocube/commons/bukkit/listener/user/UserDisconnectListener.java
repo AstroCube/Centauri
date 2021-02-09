368e2afdbb6a7ab96ae0305e8e850254e9d1783e
Ian Castaño <ianfe@martina.com.co>
2021-02-09 16:21:15 -0500

    ?

--- Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/listener/user/UserDisconnectListener.java
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/listener/user/UserDisconnectListener.java
@@ -10,6 +10,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
+import team.unnamed.uboard.ScoreboardManager;
 
 import java.util.logging.Level;
 
@@ -17,6 +18,7 @@ public class UserDisconnectListener implements Listener {
 
     private @Inject Plugin plugin;
     private @Inject MatchAssigner matchAssigner;
+    private @Inject ScoreboardManager scoreboardManager;
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onUserLeave(PlayerQuitEvent event) {
@@ -30,6 +32,12 @@ public class UserDisconnectListener implements Listener {
                 if (type == ServerDoc.Type.GAME) {
                     matchAssigner.unAssign(player);
                 }
+
+                scoreboardManager.getScoreboard(event.getPlayer().getDatabaseIdentifier()).ifPresent(board -> {
+                    scoreboardManager.removeScoreboard(player);
+                    scoreboardManager.removeScoreboard(board);
+                });
+
             } catch (Exception e) {
                 plugin.getLogger().log(Level.SEVERE, "There was an error disconnecting a player from game.", e);
             }
