8093d11390e5cf2512afcbebd65950bd5b75b7d4
Ian Castaño <ianfe@martina.com.co>
2021-02-09 16:33:49 -0500

    Exposed manager provider

--- Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/board/ScoreboardModule.java
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/board/ScoreboardModule.java
@@ -2,12 +2,14 @@ package net.astrocube.commons.bukkit.board;
 
 import me.fixeddev.inject.ProtectedModule;
 import net.astrocube.api.bukkit.board.ScoreboardManagerProvider;
+import team.unnamed.uboard.ScoreboardManager;
 
 public class ScoreboardModule extends ProtectedModule {
 
     @Override
     public void configure() {
         bind(ScoreboardManagerProvider.class).to(CoreScoreboardManagerProvider.class);
+        bind(ScoreboardManagerProvider.class);
     }
 
 }
