8b39faaee3b42bf39e8ccd83ca259cdbc9770055
Ian Castaño <ianfe@martina.com.co>
2021-02-10 11:23:01 -0500

    Replaced clickable

--- Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/menu/punishment/PunishmentReasonChooserMenu.java
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/menu/punishment/PunishmentReasonChooserMenu.java
@@ -1,62 +1,66 @@
 package net.astrocube.commons.bukkit.menu.punishment;
 
 import me.yushust.message.MessageHandler;
+import net.astrocube.api.bukkit.menu.ShapedMenuGenerator;
 import net.astrocube.api.bukkit.punishment.PresetPunishment;
 import net.astrocube.api.bukkit.punishment.PresetPunishmentCache;
 import net.astrocube.api.core.punishment.PunishmentBuilder;
-import net.astrocube.commons.bukkit.menu.MenuUtils;
-import net.astrocube.commons.bukkit.menu.punishment.helper.PunishmentReasonChooserHelper;
-import net.astrocube.commons.core.utils.Pagination;
-import net.astrocube.commons.core.utils.SimplePagination;
+import net.astrocube.commons.bukkit.menu.punishment.helper.PunishClickableGenerator;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
-import team.unnamed.gui.core.gui.GUIBuilder;
 
 import javax.inject.Inject;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class PunishmentReasonChooserMenu {
 
-    private @Inject PunishmentReasonChooserHelper punishmentReasonChooserHelper;
+    private @Inject PunishClickableGenerator punishClickableGenerator;
     private @Inject PresetPunishmentCache presetPunishmentCache;
+    private @Inject PunishmentChooserMenu punishmentChooserMenu;
     private @Inject MessageHandler messageHandler;
+    private @Inject ShapedMenuGenerator shapedMenuGenerator;
 
     public Inventory createFilter(Player player, PunishmentBuilder builder, int page) {
-
-        Pagination<PresetPunishment> presetPunishment =
-                new SimplePagination<>(28, presetPunishmentCache.getPunishments(builder.getType()));
-
-        return createChooser(player, builder, presetPunishment, "", false, page);
+        return createChooser(
+                player,
+                builder,
+                presetPunishmentCache.getPunishments(builder.getType()),
+                "",
+                false,
+                page
+        );
     }
 
     public Inventory createSearch(Player player, PunishmentBuilder builder, String search, int page) {
-
-        Pagination<PresetPunishment> presetPunishment =
-                new SimplePagination<>(
-                        28,
-                        presetPunishmentCache.getPunishments()
-                                .stream()
-                                .filter(p ->
-                                        messageHandler.get(
-                                                player,
-                                                "punish-menu.reasons." + p.getId() + ".title"
-                                        ).contains(search))
-                                .collect(Collectors.toSet())
-                );
-
-
-        return createChooser(player, builder, presetPunishment, search, true, page);
+        return createChooser(
+                player,
+                builder,
+                presetPunishmentCache.getPunishments()
+                        .stream()
+                        .filter(p ->
+                                messageHandler.get(
+                                        player,
+                                        "punish-menu.reasons." + p.getId() + ".title"
+                                ).contains(search))
+                        .collect(Collectors.toSet()),
+                search,
+                true,
+                page
+        );
     }
 
-    private Inventory createChooser(Player player, PunishmentBuilder builder, Pagination<PresetPunishment> preset, String criteria, boolean search, int page) {
-
-        GUIBuilder guiBuilder = GUIBuilder
-                .builder(messageHandler.get(player, "punishment-expiration-menu.title"), 6);
-        MenuUtils.generateFrame(guiBuilder);
-        punishmentReasonChooserHelper.buildPunishReasons(player, builder, preset, criteria, search, page)
-                .forEach(guiBuilder::addItem);
-
-        return guiBuilder.build();
+    private Inventory createChooser(Player player, PunishmentBuilder builder, Set<PresetPunishment> preset, String criteria, boolean search, int page) {
+        return shapedMenuGenerator.generate(
+                player,
+                messageHandler.get(player, "punishment-expiration-menu.title"),
+                p -> p.openInventory(punishmentChooserMenu.createPunishmentChooserMenu(
+                        player,
+                        builder.getIssuer(),
+                        builder.getTarget()
+                )),
+                punishClickableGenerator.buildPunishReasons(player, builder, preset, criteria, search)
+        );
     }
 
 }
---
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/menu/punishment/helper/PunishClickableGenerator.java
@@ -0,0 +1,80 @@
+package net.astrocube.commons.bukkit.menu.punishment.helper;
+
+import me.yushust.message.MessageHandler;
+import net.astrocube.api.bukkit.menu.ShapedMenuGenerator;
+import net.astrocube.api.bukkit.punishment.PresetPunishment;
+import net.astrocube.api.core.punishment.PunishmentBuilder;
+import net.astrocube.api.core.punishment.PunishmentHandler;
+import net.astrocube.commons.bukkit.menu.punishment.PunishmentExpirationChooserMenu;
+import net.astrocube.commons.core.utils.PrettyTimeUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import javax.inject.Inject;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class PunishClickableGenerator {
+
+    private @Inject PunishmentExpirationChooserMenu punishmentExpirationChooserMenu;
+    private @Inject MessageHandler messageHandler;
+
+    public Set<ShapedMenuGenerator.BaseClickable> buildPunishReasons
+            (Player player, PunishmentBuilder punishmentBuilder, Set<PresetPunishment> punishments, String criteria, boolean search) {
+        return punishments.stream()
+                .map(punishment -> {
+
+                    String translated = messageHandler.get(player, "punish-menu.reasons." + punishment.getId() + ".reason");
+                    ItemStack paper = new ItemStack(Material.PAPER);
+                    ItemMeta meta = paper.getItemMeta();
+
+                    meta.setDisplayName(
+                            ChatColor.AQUA +
+                                    messageHandler.get(player, "punish-menu.reasons." + punishment.getId() + ".title")  +
+                                    ChatColor.GRAY + (search ? " - " + messageHandler.get(player, "punish-menu.type." + punishment.getType().toString().toLowerCase()) : "")
+                    );
+
+                    meta.setLore(
+                            messageHandler.replacingMany(
+                                    player, "punish-menu.lore",
+                                    "%%reason%%", translated,
+                                    "%%expire%%", punishment.getExpiration() == -1 ? messageHandler.get(player, "punishment-expiration.never") :
+                                            PrettyTimeUtils.getHumanDate(
+                                                    PunishmentHandler.generateFromExpiration(
+                                                            punishment.getExpiration()
+                                                    ).toDate(),
+                                                    punishmentBuilder.getIssuer().getLanguage()
+                                            )
+                            )
+                    );
+
+                    paper.setItemMeta(meta);
+
+                    return new ShapedMenuGenerator.BaseClickable() {
+                        @Override
+                        public Consumer<Player> getClick() {
+                            return (p) -> {
+                                if (search) {
+                                    punishmentBuilder.setType(punishment.getType());
+                                }
+                                punishmentBuilder.setReason(translated);
+                                punishmentBuilder.setDuration(punishment.getExpiration());
+                                player.openInventory(punishmentExpirationChooserMenu.createPunishmentExpirationChooserMenu(player, punishmentBuilder, criteria, search));
+                            };
+                        }
+
+                        @Override
+                        public ItemStack getStack() {
+                            return paper;
+                        }
+                    };
+
+
+                }).collect(Collectors.toSet());
+    }
+
+}
