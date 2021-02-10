42bb8059d82fe043937150c89fc32a2616451b70
Ian Castaño <ianfe@martina.com.co>
2021-02-10 11:38:39 -0500

    Documented

--- API/bukkit/src/main/java/net/astrocube/api/bukkit/menu/ShapedMenuGenerator.java
+++ API/bukkit/src/main/java/net/astrocube/api/bukkit/menu/ShapedMenuGenerator.java
@@ -10,6 +10,15 @@ import java.util.function.Consumer;
 
 public interface ShapedMenuGenerator {
 
+    /**
+     * Generate default inventory for result displaying
+     * in a paginated and easier way, UX friendly.
+     * @param player where menu will be used
+     * @param title of the menu
+     * @param backClick if a back button is needed and which action will perform when clicked.
+     * @param items to be displayed
+     * @return inventory to be opened
+     */
     Inventory generate(
             Player player,
             String title,
@@ -17,6 +26,16 @@ public interface ShapedMenuGenerator {
             Set<BaseClickable> items
     );
 
+    /**
+     * Generate default inventory for result displaying
+     * in a paginated and easier way, UX friendly.
+     * @param player where menu will be used
+     * @param title of the menu
+     * @param backClick if a back button is needed and which action will perform when clicked.
+     * @param items to be displayed
+     * @param page to open
+     * @return inventory to be opened
+     */
     Inventory generate(
             Player player,
             String title,
@@ -25,10 +44,20 @@ public interface ShapedMenuGenerator {
             int page
     );
 
+    /**
+     * Compound of stack and click without
+     * explicitly have an slot
+     */
     interface BaseClickable {
 
+        /**
+         * @return action to be performed when clicked
+         */
         Consumer<Player> getClick();
 
+        /**
+         * @return stack to be used
+         */
         ItemStack getStack();
 
     }
