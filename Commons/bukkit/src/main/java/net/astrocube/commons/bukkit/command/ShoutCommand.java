d8083bf83193e68e555f15e962c2a5c06827d275
Ian Castaño <ianfe@martina.com.co>
2021-02-25 18:50:51 -0500

    Aja

--- Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/command/ShoutCommand.java
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/command/ShoutCommand.java
@@ -29,7 +29,7 @@ public class ShoutCommand implements CommandClass {
     private @Inject Plugin plugin;
 
     @Command(names = {"shout"})
-    public boolean onShoutCommand(@Sender Player player, @Text String message, @Flag("g") boolean global) {
+    public boolean onShoutCommand(@Sender Player player, @Flag("g") boolean global, @Text String message) {
 
         try {
             Optional<Match> matchOptional = actualMatchCache.get(player.getDatabaseIdentifier());
