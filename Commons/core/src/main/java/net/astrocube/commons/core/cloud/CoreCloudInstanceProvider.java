2abe1c0f426f2adf8c46bc36c9063a8eba79f1c1
Ian Castaño <ianfe@martina.com.co>
2021-02-28 23:11:25 -0500

    Updated many things

--- Commons/core/src/main/java/net/astrocube/commons/core/cloud/CoreCloudInstanceProvider.java
+++ Commons/core/src/main/java/net/astrocube/commons/core/cloud/CoreCloudInstanceProvider.java
@@ -59,9 +59,7 @@ public class CoreCloudInstanceProvider implements CloudInstanceProvider {
 
     @Override
     public boolean isAvailable(String slug) {
-        System.out.println(slug);
-        System.out.println(TimoCloudAPI.getUniversalAPI().getServer(slug));
-        return TimoCloudAPI.getUniversalAPI().getServer(slug) != null;
+        return TimoCloudAPI.getUniversalAPI().getServers().stream().anyMatch(s -> s.getId().equalsIgnoreCase(slug));
     }
 
 
