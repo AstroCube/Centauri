a65a0c41f7310a613f6f8e6a0d4a986ad19f16ca
Ian Castaño <ianfe@martina.com.co>
2021-03-04 12:06:22 -0500

    Jose

--- Commons/core/src/main/java/net/astrocube/commons/core/cloud/dummy/DummyNameProvider.java
+++ Commons/core/src/main/java/net/astrocube/commons/core/cloud/dummy/DummyNameProvider.java
@@ -3,12 +3,14 @@ package net.astrocube.commons.core.cloud.dummy;
 import com.google.inject.Singleton;
 import net.astrocube.api.core.cloud.InstanceNameProvider;
 
+import java.util.UUID;
+
 @Singleton
 public class DummyNameProvider implements InstanceNameProvider {
 
     @Override
     public String getName() {
-        return "No Cloud Hooked";
+        return UUID.randomUUID().toString();
     }
 
 }
