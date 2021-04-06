e21bab96814b5069a2a57b9083427cffff7222ce
Ian Castaño <ianfe@martina.com.co>
2021-04-05 23:03:23 -0500

    Added back button

--- Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/admin/selector/AdminGameModeSelectorMenu.java
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/admin/selector/AdminGameModeSelectorMenu.java
@@ -1,12 +1,17 @@
 package net.astrocube.commons.bukkit.admin.selector;
 
 import me.yushust.message.MessageHandler;
+import net.astrocube.api.bukkit.menu.GenericHeadHelper;
 import net.astrocube.api.core.service.query.QueryService;
 import net.astrocube.api.core.virtual.gamemode.GameMode;
+import net.astrocube.commons.bukkit.admin.AdminPanelMenu;
 import net.astrocube.commons.bukkit.admin.selector.item.GameModeItemExtractor;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.plugin.Plugin;
+import team.unnamed.gui.abstraction.item.ItemClickable;
+import team.unnamed.gui.abstraction.item.ItemClickableBuilder;
 import team.unnamed.gui.core.gui.GUIBuilder;
 
 import javax.inject.Inject;
@@ -16,6 +21,8 @@ public class AdminGameModeSelectorMenu {
     private @Inject MessageHandler messageHandler;
     private @Inject QueryService<GameMode> gameModeQueryService;
     private @Inject GameModeItemExtractor gameModeItemExtractor;
+    private @Inject GenericHeadHelper genericHeadHelper;
+    private @Inject AdminPanelMenu adminPanelMenu;
     private @Inject Plugin plugin;
 
     public void createGameModeSelectorMenu(Player player) {
@@ -27,18 +34,33 @@ public class AdminGameModeSelectorMenu {
                 .getAll()
                 .callback(response -> {
 
-                    if (response.isSuccessful() && response.getResponse().isPresent()) {
+                    response.ifSuccessful(modes -> {
 
-                        for (GameMode gameMode : response.getResponse().get().getFoundModels()) {
+                        guiBuilder.addItem(
+                                ItemClickable.builder(0).setItemStack(genericHeadHelper.backButton(player))
+                                        .setAction(action -> {
 
-                            if (!plugin.getConfig().getStringList("admin.teleport.disabled").contains(gameMode.getId())) {
+                                            if (action.getClick() == ClickType.LEFT) {
+                                                Bukkit.getScheduler().runTask(plugin, () -> adminPanelMenu.createAdminPanel(player));
+                                            }
+
+                                            return true;
+                                        })
+                                .build()
+                        );
+
+                        for (GameMode gameMode : modes.getFoundModels()) {
+
+                            if (!plugin.getConfig()
+                                    .getStringList("admin.teleport.disabled").contains(gameMode.getId())
+                            ) {
                                 guiBuilder.addItem(gameModeItemExtractor.generateGameMode(gameMode, player));
                             }
 
                         }
 
                         Bukkit.getScheduler().runTask(plugin, () -> player.openInventory(guiBuilder.build()));
-                    }
+                    });
                 });
     }
 }
