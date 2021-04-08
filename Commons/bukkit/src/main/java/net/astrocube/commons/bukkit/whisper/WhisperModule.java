58915ee1bc01f09f488deef93ba198e1b0c63052
Ian Castaño <ianfe@martina.com.co>
2021-04-08 13:38:40 -0500

    Registered channel

--- Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/whisper/WhisperModule.java
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/whisper/WhisperModule.java
@@ -1,12 +1,14 @@
 package net.astrocube.commons.bukkit.whisper;
 
 import me.fixeddev.inject.ProtectedModule;
+import net.astrocube.api.core.message.ChannelBinder;
 
-public class WhisperModule extends ProtectedModule {
+public class WhisperModule extends ProtectedModule implements ChannelBinder {
 
     @Override
     public void configure() {
         bind(WhisperManager.class).to(CoreWhisperManager.class);
+        bindChannel(WhisperMessage.class);
     }
 
 }
