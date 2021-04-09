a2b767c4b1afa3d8e17b4b235a415b80f5c56d36
Ian Castaño <ianfe@martina.com.co>
2021-04-08 21:49:40 -0500

    Removed dangling import

--- Commons/core/src/main/java/net/astrocube/commons/core/CommonsModule.java
+++ Commons/core/src/main/java/net/astrocube/commons/core/CommonsModule.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.mrbean.MrBeanModule;
 import com.google.inject.Scopes;
