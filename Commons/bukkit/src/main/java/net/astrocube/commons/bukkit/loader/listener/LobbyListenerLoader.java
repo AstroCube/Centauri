61843ef7020f3e2d5405f09ad6dcd2e6f6f47867
Benjamin <supremobenjamin.cia@gmail.com>
2021-04-11 12:48:02 -0400

    A

--- Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/loader/listener/LobbyListenerLoader.java
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/loader/listener/LobbyListenerLoader.java
@@ -5,6 +5,7 @@ import net.astrocube.api.bukkit.server.ListenerLoader;
 import net.astrocube.commons.bukkit.game.spectator.LobbyActionListener;
 import net.astrocube.commons.bukkit.listener.game.session.LobbyUserDisconnectListener;
 import net.astrocube.commons.bukkit.listener.game.spectator.LobbyReturnListener;
+import net.astrocube.commons.bukkit.listener.user.UserJoinAssignFlyListener;
 import org.bukkit.plugin.Plugin;
 
 public class LobbyListenerLoader implements ListenerLoader {
@@ -14,6 +15,7 @@ public class LobbyListenerLoader implements ListenerLoader {
     private @Inject LobbyUserDisconnectListener lobbyUserDisconnectListener;
     private @Inject LobbyReturnListener lobbyReturnListener;
     private @Inject LobbyActionListener lobbyActionListener;
+    private @Inject UserJoinAssignFlyListener userJoinAssignFlyListener;
 
     @Override
     public void registerEvents() {
@@ -21,7 +23,8 @@ public class LobbyListenerLoader implements ListenerLoader {
                 plugin,
                lobbyUserDisconnectListener,
                 lobbyReturnListener,
-                lobbyActionListener
+                lobbyActionListener,
+                userJoinAssignFlyListener
         );
     }
 
