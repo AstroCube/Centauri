5da6f7fd8ef418bb14b7517912b7ff22a841d1d8
yusshu <yushu@unnamed.team>
2021-05-21 18:27:30 -0500

    fix

--- Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/whisper/WhisperListener.java
+++ Commons/bukkit/src/main/java/net/astrocube/commons/bukkit/whisper/WhisperListener.java
@@ -1,17 +1,15 @@
 package net.astrocube.commons.bukkit.whisper;
 
+import com.google.inject.Inject;
 import me.fixeddev.minecraft.player.Player;
+import me.yushust.message.MessageHandler;
 import net.astrocube.api.core.message.MessageListener;
 import net.astrocube.api.core.message.MessageMetadata;
 import org.bukkit.Bukkit;
 
 public class WhisperListener implements MessageListener<WhisperMessage> {
 
-	private final me.yushust.message.MessageHandler messageHandler;
-
-	WhisperListener(me.yushust.message.MessageHandler messageHandler) {
-		this.messageHandler = messageHandler;
-	}
+	@Inject private MessageHandler messageHandler;
 
 	@Override
 	public void handleDelivery(WhisperMessage message, MessageMetadata properties) {
