9c09731f4758aa14972ade94e786148d3bdaae90
yusshu <yushu@unnamed.team>
2021-05-22 01:57:24 -0500

    debug

--- Lobby/src/main/java/net/astrocube/lobby/selector/gamemode/CoreGameItemExtractor.java
+++ Lobby/src/main/java/net/astrocube/lobby/selector/gamemode/CoreGameItemExtractor.java
@@ -32,12 +32,17 @@ public class CoreGameItemExtractor implements GameItemExtractor {
 		List<String> baseLore = new ArrayList<>(messageHandler.getMany(player, "lobby.gameSelector.games." + gameModeDoc.getId() + ".lore").getContents());
 
 		baseLore.add(" ");
-		baseLore.add(
-			messageHandler.replacing(
-				player, "lobby.gameSelector.gadget-playing",
-				"%players%", cloudModeConnectedProvider.getGlobalOnline(gameModeDoc)
-			)
-		);
+		try {
+			baseLore.add(
+				messageHandler.replacing(
+					player, "lobby.gameSelector.gadget-playing",
+					"%players%", cloudModeConnectedProvider.getGlobalOnline(gameModeDoc)
+				)
+			);
+		} catch (Exception e) {
+			System.out.println("error while generating " + gameModeDoc.getName() + " item");
+			e.printStackTrace();
+		}
 
 		iconMeta.setDisplayName(
 			messageHandler.get(player, "lobby.gameSelector.games." + gameModeDoc.getId() + ".title")
