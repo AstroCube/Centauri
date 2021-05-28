4527d7e4ab6884ee5db90f9698d3bee904f012e3
yusshu <yushu@unnamed.team>
2021-05-28 01:07:46 -0500

    exception logging should be handled by callers

--- Commons/core/src/main/java/net/astrocube/commons/core/http/CoreHttpClient.java
+++ Commons/core/src/main/java/net/astrocube/commons/core/http/CoreHttpClient.java
@@ -15,12 +15,10 @@ import net.astrocube.commons.core.http.request.RequestContent;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class CoreHttpClient implements HttpClient {
 
-	private final Logger logger;
 	private final HttpClientConfig httpClientConfig;
 	private final HttpRequestFactory requestFactory;
 	private final AuthorizationProcessor authorizationProcessor;
@@ -32,50 +30,40 @@ public class CoreHttpClient implements HttpClient {
 		HttpClientConfig httpClientConfig,
 		AuthorizationProcessor authorizationProcessor
 	) {
-		this.logger = Logger.getLogger(getClass().getName());
+		Logger logger = Logger.getLogger(getClass().getName());
 		this.requestFactory = factoryResolver.configureFactory();
 		this.httpClientConfig = httpClientConfig;
 		this.authorizationProcessor = authorizationProcessor;
-		transportLoggerModifier.overrideDefaultLogger(this.logger);
+		transportLoggerModifier.overrideDefaultLogger(logger);
 	}
 
 
 	@Override
 	public <T> T executeRequestSync(String path, RequestCallable<T> returnType, RequestOptions options) throws Exception {
-		try {
-			HttpRequest request = buildRequest(
-				options,
-				httpClientConfig.getBaseURL(),
-				path
-			);
+		HttpRequest request = buildRequest(
+			options,
+			httpClientConfig.getBaseURL(),
+			path
+		);
 
-			options.getHeaders().forEach((key, value) -> request.getHeaders().set(key, value));
-			request.getHeaders().setAccept("application/json");
-			request.getHeaders().set("Authorization", "Bearer " + new String(authorizationProcessor.getAuthorizationToken()));
-			return returnType.call(request);
-		} catch (Exception e) {
-			logger.log(Level.SEVERE, "Failed to build request to " + path, e);
-			throw e;
-		}
+		options.getHeaders().forEach((key, value) -> request.getHeaders().set(key, value));
+		request.getHeaders().setAccept("application/json");
+		request.getHeaders().set("Authorization", "Bearer " + new String(authorizationProcessor.getAuthorizationToken()));
+		return returnType.call(request);
 	}
 
 	@Override
 	public <T> T executeHeadlessRequestSync(String path, RequestCallable<T> returnType, RequestOptions options) throws Exception {
-		try {
-			HttpRequest request = buildRequest(
-				options,
-				path,
-				""
-			);
+		HttpRequest request = buildRequest(
+			options,
+			path,
+			""
+		);
 
-			options.getHeaders().forEach((key, value) -> request.getHeaders().set(key, value));
-			request.getHeaders().setAccept("application/json");
-			request.getHeaders().set("Authorization", "Bearer " + new String(authorizationProcessor.getAuthorizationToken()));
-			return returnType.call(request);
-		} catch (Exception e) {
-			logger.log(Level.SEVERE, "Failed to build request to " + path, e);
-			throw e;
-		}
+		options.getHeaders().forEach((key, value) -> request.getHeaders().set(key, value));
+		request.getHeaders().setAccept("application/json");
+		request.getHeaders().set("Authorization", "Bearer " + new String(authorizationProcessor.getAuthorizationToken()));
+		return returnType.call(request);
 	}
 
 	private HttpRequest buildRequest(RequestOptions options, String baseUrl, String path)
