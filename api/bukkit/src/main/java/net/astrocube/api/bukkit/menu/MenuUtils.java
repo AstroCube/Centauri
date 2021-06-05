f9c1587fa5e46d82e39c1f10b1f775e93f89f9c4
pixeldev <amiranda8695@gmail.com>
2021-06-05 18:28:36 -0500

    fix profile field reflection and remove unnecessary fields.

--- api/bukkit/src/main/java/net/astrocube/api/bukkit/menu/MenuUtils.java
+++ api/bukkit/src/main/java/net/astrocube/api/bukkit/menu/MenuUtils.java
@@ -4,56 +4,58 @@ import net.astrocube.api.bukkit.user.profile.AbstractGameProfile;
 import net.astrocube.api.bukkit.user.profile.CoreGameProfile;
 import net.astrocube.api.bukkit.user.profile.CoreProperty;
 import net.astrocube.api.bukkit.user.profile.MojangProfileGeneratorUtil;
+
+import org.bukkit.DyeColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
-import team.unnamed.gui.abstraction.item.ItemClickable;
-import team.unnamed.gui.core.gui.type.GUIBuilder;
+
+import team.unnamed.gui.core.gui.factory.GUIFactory;
+import team.unnamed.gui.core.item.type.ItemBuilder;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
 import java.util.UUID;
 
 public class MenuUtils {
 
-	public static int[] slots = new int[]{9, 17, 18, 26, 27, 35, 36, 44};
+	private static final Field PROFILE_FIELD;
+	private static final boolean PROFILE_FIELD_ACCESSIBLE;
 
-	public static boolean isMarkedSlot(int slot) {
-		return (slot >= 0 && slot < 9) || (slot > 44 && slot < 54) || Arrays.stream(slots).anyMatch(s -> s == slot);
-	}
+	static {
+		try {
+			Class<?> metaClass = Class.forName("org.bukkit.craftbukkit.v" + GUIFactory.SERVER_VERSION + ".inventory.CraftMetaSkull");
 
-	public static ItemStack generateStainedPane() {
-		ItemStack stack = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 7);
-		ItemMeta meta = stack.getItemMeta();
-		meta.setDisplayName(" ");
-		stack.setItemMeta(meta);
-		return stack;
-	}
-
-	public static void generateFrame(GUIBuilder builder) {
-		for (int i = 0; i < 54; i++) {
-			if (MenuUtils.isMarkedSlot(i)) {
-				builder.addItem(ItemClickable.builder(i).setItemStack(MenuUtils.generateStainedPane()).build());
-			}
+			PROFILE_FIELD = metaClass.getDeclaredField("profile");
+			PROFILE_FIELD_ACCESSIBLE = PROFILE_FIELD.isAccessible();
+		} catch (ClassNotFoundException | NoSuchFieldException e) {
+			throw new IllegalStateException("Cannot get the SkullMeta profile field!", e);
 		}
 	}
 
-	public static ItemStack generateHead(HeadLibrary library) {
+	public static ItemStack generateStainedPane() {
+		return ItemBuilder.newDyeItemBuilder(
+			Material.STAINED_GLASS_PANE,
+			DyeColor.GRAY
+		)
+			.setName(" ")
+			.build();
+	}
 
+	public static ItemStack generateHead(HeadLibrary library) {
 		ItemStack head = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
-		Field profileField;
 		SkullMeta meta = (SkullMeta) head.getItemMeta();
 
 		AbstractGameProfile profile = new CoreGameProfile(UUID.randomUUID(), "head");
 		profile.getProperties().add(new CoreProperty("textures", library.getBase64()));
 
+		PROFILE_FIELD.setAccessible(true);
+
 		try {
-			profileField = meta.getClass().getDeclaredField("profile");
-			profileField.setAccessible(true);
-			profileField.set(meta, MojangProfileGeneratorUtil.generateProfile(profile));
-		} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+			PROFILE_FIELD.set(meta, MojangProfileGeneratorUtil.generateProfile(profile));
+		} catch (IllegalAccessException e) {
 			e.printStackTrace();
+		} finally {
+			PROFILE_FIELD.setAccessible(PROFILE_FIELD_ACCESSIBLE);
 		}
 
 		head.setItemMeta(meta);
