79c87a85167122620a1d0b17db641d18b9d442dc
yusshu <yushu@unnamed.team>
2021-06-05 01:47:42 -0500

    commons(scoreboard): remove scoreboard on quit

--- commons/bukkit/src/main/java/net/astrocube/commons/bukkit/listener/user/UserDisconnectListener.java
+++ commons/bukkit/src/main/java/net/astrocube/commons/bukkit/listener/user/UserDisconnectListener.java
@@ -1,6 +1,7 @@
 package net.astrocube.commons.bukkit.listener.user;
 
 import com.google.inject.Inject;
+import net.astrocube.api.bukkit.board.BoardProvider;
 import net.astrocube.api.bukkit.board.ScoreboardManagerProvider;
 import net.astrocube.api.bukkit.game.match.MatchAssigner;
 import net.astrocube.api.core.virtual.server.ServerDoc;
@@ -17,6 +18,7 @@ public class UserDisconnectListener implements Listener {
 
 	private @Inject Plugin plugin;
 	private @Inject MatchAssigner matchAssigner;
+	private @Inject BoardProvider boardProvider;
 	private @Inject ScoreboardManagerProvider scoreboardManagerProvider;
 
 	@EventHandler(priority = EventPriority.LOWEST)
@@ -31,6 +33,8 @@ public class UserDisconnectListener implements Listener {
 			if (type == ServerDoc.Type.GAME) {
 				matchAssigner.unAssign(player);
 			}
+
+			boardProvider.delete(player);
 
 			scoreboardManagerProvider.getScoreboard().getScoreboard(event.getPlayer().getDatabaseIdentifier()).ifPresent(board -> {
 				scoreboardManagerProvider.getScoreboard().removeScoreboard(player);
