aa4d8bee779a8f438665caf03cf4eef2aabdee98
yusshu <yushu@unnamed.team>
2021-06-12 22:39:07 -0500

    grant execution permissions to wrapper scripts and remove unused imports

--- lobby/src/main/java/net/astrocube/lobby/selector/lobby/CoreLobbyCloudWrapperGenerator.java
+++ lobby/src/main/java/net/astrocube/lobby/selector/lobby/CoreLobbyCloudWrapperGenerator.java
@@ -8,8 +8,6 @@ import net.astrocube.api.core.virtual.gamemode.GameMode;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Singleton
