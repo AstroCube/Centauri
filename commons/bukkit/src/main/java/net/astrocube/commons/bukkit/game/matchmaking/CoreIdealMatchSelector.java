49b653f7c267da4647407ad97a2b848ecbeeda70
yusshu <yushu@unnamed.team>
2021-06-19 17:45:15 -0500

    fix ideal match selector

--- commons/bukkit/src/main/java/net/astrocube/commons/bukkit/game/matchmaking/CoreIdealMatchSelector.java
+++ commons/bukkit/src/main/java/net/astrocube/commons/bukkit/game/matchmaking/CoreIdealMatchSelector.java
@@ -2,18 +2,23 @@ package net.astrocube.commons.bukkit.game.matchmaking;
 
 import com.google.inject.Singleton;
 import net.astrocube.api.bukkit.game.matchmaking.IdealMatchSelector;
+import net.astrocube.api.bukkit.game.matchmaking.MatchAssignable;
 import net.astrocube.api.bukkit.virtual.game.match.Match;
 
 import java.util.Comparator;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collector;
 
 @Singleton
 public class CoreIdealMatchSelector implements IdealMatchSelector {
 
 	@Override
 	public Optional<Match> sortAvailableMatches(Set<Match> matches) {
-		return matches.stream().max(Comparator.comparingInt(o -> o.getSpectators().size()));
+		return matches.stream().max(Comparator.comparingInt(o ->
+				o.getPending().stream()
+						.mapToInt(pending -> pending.getInvolved().size() + 1 /* the responsible */)
+						.sum()));
 	}
 
 }
