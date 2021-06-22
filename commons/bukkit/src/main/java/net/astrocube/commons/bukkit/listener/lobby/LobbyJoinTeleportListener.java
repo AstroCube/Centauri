e5546bd8ab0d4fcdb715874eef53d4c286133799
Benjamin <supremobenjamin.cia@gmail.com>
2021-06-22 00:01:52 -0400

    just teleporting

--- commons/bukkit/src/main/java/net/astrocube/commons/bukkit/listener/lobby/LobbyJoinTeleportListener.java
+++ commons/bukkit/src/main/java/net/astrocube/commons/bukkit/listener/lobby/LobbyJoinTeleportListener.java
@@ -2,7 +2,6 @@ package net.astrocube.commons.bukkit.listener.lobby;
 
 import net.astrocube.api.bukkit.lobby.event.LobbyJoinEvent;
 import net.astrocube.commons.bukkit.game.match.lobby.LobbyLocationParser;
-import net.astrocube.commons.bukkit.utils.TeleportUtils;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -10,7 +9,7 @@ public class LobbyJoinTeleportListener implements Listener {
 
 	@EventHandler
 	public void onJoinLobby(LobbyJoinEvent event) {
-		TeleportUtils.loadChunkAndTeleport(event.getPlayer(), LobbyLocationParser.getLobby());
+		event.getPlayer().teleport(LobbyLocationParser.getLobby());
 	}
 
 }
