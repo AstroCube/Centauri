c4a011be9fd578cb98071ca679196aa02b98bca5
Benjamin <supremobenjamin.cia@gmail.com>
2021-06-23 12:59:41 -0400

    I forgot to send the message

--- api/bukkit/src/main/java/net/astrocube/api/bukkit/server/broadcast/GlobalBroadcaster.java
+++ api/bukkit/src/main/java/net/astrocube/api/bukkit/server/broadcast/GlobalBroadcaster.java
@@ -1,7 +1,9 @@
 package net.astrocube.api.bukkit.server.broadcast;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 public interface GlobalBroadcaster {
 
-	void broadcastMessage(String message);
+	void broadcastMessage(String message) throws JsonProcessingException;
 
 }
--- commons/bukkit/src/main/java/net/astrocube/commons/bukkit/server/broadcast/CoreGlobalBroadcaster.java
+++ commons/bukkit/src/main/java/net/astrocube/commons/bukkit/server/broadcast/CoreGlobalBroadcaster.java
@@ -1,14 +1,26 @@
 package net.astrocube.commons.bukkit.server.broadcast;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.inject.Inject;
 import net.astrocube.api.bukkit.server.broadcast.GlobalBroadcaster;
+import net.astrocube.api.core.message.Channel;
+import net.astrocube.api.core.message.Messenger;
+
+import java.util.HashMap;
 
 public class CoreGlobalBroadcaster implements GlobalBroadcaster {
 
+	private final Channel<BroadcastMessage> broadcastMessageChannel;
 
+	@Inject
+	public CoreGlobalBroadcaster(Messenger messenger) {
+		broadcastMessageChannel = messenger.getChannel(BroadcastMessage.class);
+	}
 
 	@Override
-	public void broadcastMessage(String message) {
-
+	public void broadcastMessage(String message) throws JsonProcessingException {
+		broadcastMessageChannel
+			.sendMessage(new BroadcastMessage(message), new HashMap<>());
 	}
 
 }
