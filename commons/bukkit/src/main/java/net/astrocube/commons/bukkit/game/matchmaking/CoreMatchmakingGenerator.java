06ab9da91fb1ad5034e6c756f1ae10568c08d759
Benjamin <supremobenjamin.cia@gmail.com>
2021-06-30 00:01:51 -0400

    fixed bug with the matchmaking in duplicate pending in match

--- commons/bukkit/src/main/java/net/astrocube/commons/bukkit/game/matchmaking/CoreMatchmakingGenerator.java
+++ commons/bukkit/src/main/java/net/astrocube/commons/bukkit/game/matchmaking/CoreMatchmakingGenerator.java
@@ -1,14 +1,16 @@
 package net.astrocube.commons.bukkit.game.matchmaking;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import me.yushust.message.MessageHandler;
 import net.astrocube.api.bukkit.game.matchmaking.MatchAssignable;
 import net.astrocube.api.bukkit.game.matchmaking.MatchmakingGenerator;
 import net.astrocube.api.bukkit.game.matchmaking.MatchmakingRegistryHandler;
+import net.astrocube.api.core.redis.Redis;
 import net.astrocube.api.core.virtual.gamemode.GameMode;
 import net.astrocube.api.core.virtual.gamemode.SubGameMode;
 import org.bukkit.entity.Player;
+import redis.clients.jedis.Jedis;
 
 import java.util.HashSet;
 
@@ -16,9 +18,23 @@ import java.util.HashSet;
 public class CoreMatchmakingGenerator implements MatchmakingGenerator {
 
 	private @Inject MatchmakingRegistryHandler matchmakingRegistryHandler;
+	private @Inject Redis redis;
+	private @Inject MessageHandler messageHandler;
 
 	@Override
 	public void pairMatch(Player player, GameMode mode, SubGameMode subMode) throws Exception {
+
+		String id = player.getUniqueId().toString();
+
+		try (Jedis jedis = redis.getRawConnection().getResource()) {
+			if (jedis.exists("Cool-down-Request-Game:" + id)) {
+				messageHandler.send(player, "play.matchmaking-request-cool-down");
+				return;
+			}
+
+			jedis.set("Cooldown-Request-Game:" + id, "true");
+			jedis.expire("Cool-down-Request-Game:" + id, 5);
+		}
 
 		MatchAssignable assignable = new MatchAssignable(
 			player.getDatabaseIdentifier(),
