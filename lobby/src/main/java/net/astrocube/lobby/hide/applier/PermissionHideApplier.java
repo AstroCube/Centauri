c906decf5cce3de9fcdb8fa03536acaa6216a514
Solotory <Joshua1504.jr@gmail.com>
2021-07-06 09:10:46 -0400

    removed the run-task function, because these methods are called in the bukkit thread

--- lobby/src/main/java/net/astrocube/lobby/hide/applier/PermissionHideApplier.java
+++ lobby/src/main/java/net/astrocube/lobby/hide/applier/PermissionHideApplier.java
@@ -1,25 +1,21 @@
 package net.astrocube.lobby.hide.applier;
 
-import com.google.inject.Inject;
 import com.google.inject.Singleton;
+
 import net.astrocube.api.bukkit.lobby.hide.HideApplier;
 import net.astrocube.api.core.virtual.user.User;
-import org.bukkit.Bukkit;
+
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 
 @Singleton
 public class PermissionHideApplier implements HideApplier {
 
-
-	private @Inject
-	Plugin plugin;
-
 	@Override
 	public void apply(User user, Player player, User target, Player targetPlayer) {
 		if (targetPlayer.hasPermission("lobby.hide.bypass")) {
-			Bukkit.getScheduler().runTask(plugin, () -> player.showPlayer(targetPlayer));
+			player.showPlayer(targetPlayer);
+		} else {
+			player.hidePlayer(targetPlayer);
 		}
 	}
-
 }
