fdc34b4a8191bbbb595113a815a95a8ee68ced62
Benjamin <supremobenjamin.cia@gmail.com>
2021-07-13 16:18:45 -0400

    registered listener game again

--- commons/bukkit/src/main/java/net/astrocube/commons/bukkit/loader/listener/game/SpectatorListenerLoader.java
+++ commons/bukkit/src/main/java/net/astrocube/commons/bukkit/loader/listener/game/SpectatorListenerLoader.java
@@ -2,6 +2,7 @@ package net.astrocube.commons.bukkit.loader.listener.game;
 
 import com.google.inject.Inject;
 import net.astrocube.api.bukkit.server.ListenerLoader;
+import net.astrocube.commons.bukkit.listener.game.spectator.GameAgainListener;
 import net.astrocube.commons.bukkit.listener.game.spectator.SpectateRequestListener;
 import net.astrocube.commons.bukkit.listener.game.spectator.SpectatorAssignListener;
 import org.bukkit.plugin.Plugin;
@@ -12,13 +13,15 @@ public class SpectatorListenerLoader implements ListenerLoader {
 
 	private @Inject SpectatorAssignListener spectatorAssignListener;
 	private @Inject SpectateRequestListener spectateRequestListener;
+	private @Inject GameAgainListener gameAgainListener;
 
 	@Override
 	public void registerEvents() {
 		registerEvent(
 			plugin,
 			spectatorAssignListener,
-			spectateRequestListener
+			spectateRequestListener,
+			gameAgainListener
 		);
 	}
 
